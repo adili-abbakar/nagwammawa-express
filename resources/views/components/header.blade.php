d586dea37614b72eaf10addf445f6eed886fbe75
Abubakar Ahmad Adili <abubakarahmadadili@gmail.com>
2026-03-02 05:23:28 +0100

    feat(looks): add logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,8 @@
 <header id="header">
-    <a class="logo" href="{{ route('home') }}">Nagwammawa Express</a>
+    <a href="{{ route('home') }}" class="logo-container">
+        <img class="logo" src="{{ asset('images/logo.png') }}" alt="">
+    </a>
+    {{-- <a class="logo" href="{{ route('home') }}">Nagwammawa Express</a> --}}
     <nav>
         <a href="#menu">Menu</a>
     </nav>
